dad644dbc7a670362f39a86e2a86fb516e335331
ostlight <rhainich@astro.physik.uni-potsdam.de>
2026-01-15 08:09:17 +0100

    Set new default options

--- n2/1_add_images.py
+++ n2/1_add_images.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 
 """
-    First part of the reduction pipeline for data taken within the
-    scope of the N2 observation of the astrophysics lab course at
-    Potsdam University.
+First part of the reduction pipeline for data taken within the
+scope of the N2 observation of the astrophysics lab course at
+Potsdam University.
 
-    All files can be given in one directory called 'raw_files'. Alternatively
-    images can be sorted into the following directory structure:
-        * Images of the object
-        * Dark frames
-        * Flatfields
-    If they are sorted into directories the FITS header keywords will be
-    checked for consistency.
+All files can be given in one directory called 'raw_files'. Alternatively
+images can be sorted into the following directory structure:
+    * Images of the object
+    * Dark frames
+    * Flatfields
+If they are sorted into directories the FITS header keywords will be
+checked for consistency.
 
-    Images in sub folders will be recognized, but only one level is considered.
+Images in sub folders will be recognized, but only one level is considered.
 """
 
 ############################################################################
@@ -22,7 +22,7 @@
 ############################################################################
 #   PLEASE NOTE: Either specify a single directory with all raw files at
 #                this point, or use the directory structure shown below.
-raw_files: str = '?'
+raw_files: str = "?"
 
 ############################################################################
 #                           Individual folders                             #
@@ -31,16 +31,16 @@ raw_files: str = '?'
 #   single directory with all raw files above.
 
 # Path to the bias -- If set to '?', bias exposures are not used.
-bias: str = '?'
+bias: str = "?"
 
 # Path to the darks
-darks: str = '?'
+darks: str = "?"
 
 # Path to the flats
-flats: str = '?'
+flats: str = "?"
 
 # Path to the images
-images: str = '?'
+images: str = "?"
 
 
 ############################################################################
@@ -55,17 +55,17 @@ target_name: str | None = None
 
 #   Path to store the output (will usually be 'output',
 #   but it can be changed as needed).
-output_dir: str = 'output/'
+output_dir: str = "output/"
 
 #   Remove cosmic rays?
 rm_cosmic_rays: bool = True
 # rm_cosmic_rays: bool = False
 
 #   Tolerance between science and dark exposure times in s
-exposure_time_tolerance: float = 5.
+exposure_time_tolerance: float = 5.0
 
 #   Tolerance between the camera chip temperatures of the images
-temperature_tolerance: float = 5.
+temperature_tolerance: float = 5.0
 
 #   Number of cores used for multiprocessing
 n_cores_multiprocessing: int = 4
@@ -75,26 +75,24 @@ n_cores_multiprocessing: int = 4
 #                               Libraries                                  #
 ############################################################################
 
-import time
-
 import tempfile
-
+import time
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 from astropy import log
-log.setLevel('ERROR')
 
-from ost_photometry.reduce import redu
-from ost_photometry.reduce import utilities
+log.setLevel("ERROR")
+
 from ost_photometry import style
-
+from ost_photometry.reduce import redu, utilities
 
 ############################################################################
 #                                  Main                                    #
 ############################################################################
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     #   Set start time
     start_time = time.time()
 
@@ -126,10 +124,12 @@ if __name__ == '__main__':
         target_name=target_name,
         temperature_tolerance=temperature_tolerance,
         n_cores_multiprocessing=n_cores_multiprocessing,
+        shift_method="aa_true",
+        shift_all=True,
     )
 
     print(style.Bcolors.OKGREEN + "   Done" + style.Bcolors.ENDC)
-    print("--- %s minutes ---" % ((time.time() - start_time) / 60.))
+    print("--- %s minutes ---" % ((time.time() - start_time) / 60.0))
 
 """
     Change Log
